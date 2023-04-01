73631d0e7203c5d566ba33dc4ac7a20064d8c336
Lehi Cano <onacihel@gmail.com>
2023-04-01 13:19:46 -0500

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import '../styles/app.scss';
+</script>
+
+<svelte:head>
+	<title>Test your type</title>
+</svelte:head>
+
+<div class="layout">
+	<nav>
+		<h1>Typer</h1>
+	</nav>
+
+	<main>
+		<slot />
+	</main>
+</div>
+
+<style lang="scss">
+	h1 {
+		font-family: 'loos-extended', sans-serif;
+		font-size: 1.8rem;
+		color: var(--fg-200);
+		letter-spacing: 2px;
+	}
+
+	.layout {
+		height: 100%;
+		display: grid;
+		grid-template-rows: auto 1fr;
+		align-items: center;
+		padding: 2rem;
+	}
+</style>
